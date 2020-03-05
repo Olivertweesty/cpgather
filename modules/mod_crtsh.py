c6fce974e2cef43e1369fc051378296dc75b9716
Douglas Santos <douglas.secco@gmail.com>
2020-03-05 15:21:21 -0300

    adding resume support

--- modules/mod_crtsh.py
+++ modules/mod_crtsh.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from misc import readFile, saveFile
 from crtsh import crtshAPI
+
 
 def crtsh(domain):
     found = list()
-    rawlist = crtshAPI().search(domain)[0]
-    for item in rawlist:
-        for k,v in item.items():
-            if k == "name_value":
-                if '\n' in v:
-                    for tok in v.split('\n'):
-                        found.append(tok)
-                else:
-                    found.append(v)
+    if os.path.isfile(domain + ".sub.crtsh") == False or os.path.getsize(domain + ".sub.crtsh") == 0:
 
-    found = list(set(found))
+        rawlist = crtshAPI().search(domain)[0]
+        for item in rawlist:
+            for k, v in item.items():
+                if k == "name_value":
+                    if '\n' in v:
+                        for tok in v.split('\n'):
+                            found.append(tok)
+                    else:
+                        found.append(v)
+
+        found = list(set(found))
+        saveFile(domain + ".sub.crtsh", found)
+
+    else:
+
+        temp = readFile(domain + ".sub.crtsh")
+        for item in temp:
+            if len(item) > 2:
+                found.append(item.rstrip("\n"))
     return found
